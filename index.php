37344c46137b962b3bd6e88945b15d0ecad33e12
G- <gingerjimenez213@gmail.com>
2022-07-20 16:53:12 -0500

    Se sube el proyecto a la nube

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>Venta de vehiculo |</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+       
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/login.css">
+     </head>          
+    
+        
+     <body style="background: #922ff0" >
+     
+        <!--<div class="col-lg-5 col-lg-offset-6"></div>--> 
+        <div class="container">
+            <div class="login-form">
+                
+            <form class="form-register">
+                 <strong> vehiculo</strong>  <input class="form-control" type="text" id="vehiculo" name="Usuario" placeholder="ingrese el nombre del vehiculo" required>
+                <strong>Modelo</strong><input class="form-control" type="text" id="modelo" name="modelo"placeholder="ingrese el nombre del modelo">
+                <strong> Cantidad</strong>  <input class="form-control" type="number" id="vehiculo" name="cantidad" placeholder="ingrese la cantidad" required>
+                <strong>Precio</strong><input class="form-control" type="text" id="precio" name="precio"placeholder="ingrese el precio">
+                <strong>Consecionario</strong><input class="form-control" type="text" id="descrpcion" name="descripcion"placeholder="ingrese la descripcion ">
+<!--<a class="glyphicon glyphicon-user" href="index.php">Olvidé Contraseña</a>-->
+                <button id="ingresarBtn" name="Ingresar" class="btn btn-block bt-login" >   Calcular el monto a pagar </button>
+                </form>
+                
+                <!--<div id="mensaje"></div>-->
+
+            </div>
+        </div>
+    </body>
+</html>
